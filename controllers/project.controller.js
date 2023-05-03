9887a557898cbdf4e33bf2b64869333bc9bbf054
guptamanas149@gmail.com <guptamanas149@gmail.com>
2023-05-03 15:14:35 +0530

    added project page

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -1,9 +1,9 @@
-import project from "../models/course.model.js";
+import project from "../models/project.model.js";
 import { response_200, response_500 } from "../utils/response_codes.js";
 export const getAllProjects = async (req, res) => {
     try {
         const projectData = await project.find();
-        return response_200(res, "Courses fetched successfully", projectData);
+        return response_200(res, "Projects fetched successfully", projectData);
     } catch (err) {
         return response_500(res, "Error fetching courses", err);
     }
